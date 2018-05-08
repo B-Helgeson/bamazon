726b937852b18da65403a6f6babef36cb8010da2
B-Helgeson <brandonstevenhelgeson@gmail.com>
2018-05-07 22:28:50 -0400

    updated javascript, all customer functions working.

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -7,7 +7,7 @@ const   mysql = require("mysql"),
 // Defining the mysql connection variable
 var connection = mysql.createConnection({
   host: "localhost",
-  port: 3306,
+  port: 3307,
   user: "root",
   password: "root",
   database: "bamazonDB"
@@ -52,13 +52,77 @@ function start() {
       message: "Great, how many of that product would you like?"
     }])
     .then(function(answers) {
-      // Run function to submit a purchase after questions are answered
-      if (answers.item === "1") {
-        console.log("Awesome Sauce")
-      }
-      else {
-        console.log("Maybe Next Time")
-      }
+    // Return product based on input
+    getPrice(answers.item,answers.qty);
     });
   })
+}
+
+
+
+function getPrice(item,qty) {
+  console.log("Getting your total...\n");
+  connection.query("SELECT * FROM products WHERE item_id = ?", item, function(err, res) {
+   
+  // error handling
+    if (err) throw err;
+
+  // check that the item exists
+    if (res.length < 1) {
+			console.log('That product ID is not in our databse, try again');
+			start();
+    }
+  
+  // check for quantity available
+    else if (qty > res[0].stock_quantity) { 
+      console.log('Not enough inventory, please try again');
+      start();
+    }
+
+    else {
+
+      let sum = res[0].offer_price * parseInt(qty)
+
+      inquirer
+      .prompt({
+        name: "purchaseok",
+        type: "confirm",
+        message: "The total is " + sum + " , is that OK?",
+      })
+      .then(function(response) {
+      
+        // If they say no to the price...
+        if (response === "No") {
+          console.log("sorry to hear that, please come again soon!");
+          start();
+        } 
+
+        // Otherwise reduce inventory for the purchased item
+        reduceInventory(res[0].item_id, parseInt(qty))
+
+      });
+    };
+  });
+};
+
+function reduceInventory(itemId, qtyReduce) {
+  connection.query('SELECT stock_quantity from products where item_id = ?', itemId, function(err, result){
+    
+    if (err) throw err;
+
+    let currentQty = result[0].stock_quantity;
+    let newQty = currentQty - qtyReduce;
+    updateFunction(newQty);
+  })
+
+  function updateFunction(newQty){
+    connection.query('UPDATE products SET stock_quantity = ? where item_id = ?', [newQty, itemId], function(err,result){
+
+      if (err) throw err;
+
+      console.log("updating quantities...");
+
+      start();
+    })
+  }
 }
